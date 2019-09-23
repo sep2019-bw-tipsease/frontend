9db2a0865881be9d9d259f7567a00f89f6124663
jtmccutcheon <jt.mccutcheon@live.com>
2019-09-23 16:13:59 -0400

    Home.js rendering Login and added button link to register

--- tipsease/src/App.js
+++ tipsease/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-
+import Home from './components/Home'
 import './App.css';
 import Register from './components/Register';
+import { Route } from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
-      {/* <Register /> */}
+
+
+      <Route exact path='/' component={Home} />
+      <Route path='/register' component={Register} />
     </div>
   );
 }
--- tipsease/src/components/Login.js
+++ tipsease/src/components/Login.js
@@ -1,8 +1,60 @@
 import React from 'react'
+import {
+    Container, Col, Form,
+    FormGroup, Label, Input,
+    Button,
+} from 'reactstrap';
+import { Link } from 'react-router-dom'
 
 const Login = () => {
+
+
+
+
+    const userLogin = e => {
+
+    }
+
+    const handleChange = e => {
+
+    }
     return (
         <>
+
+            <div>
+                <Container className="reg-page">
+                    <Form className="form" onSubmit={userLogin}>
+                        <Col>
+                            <FormGroup>
+
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>Email</Label>
+                                <Input
+                                    type="text"
+                                    name="username"
+                                    id="exampleEmail"
+                                    placeholder="username"
+                                    onChange={handleChange}
+                                />
+                            </FormGroup>
+                        </Col>
+                        <Col>
+                            <FormGroup>
+                                <Label for="examplePassword">Password</Label>
+                                <Input
+                                    type="password"
+                                    name="password"
+                                    id="examplePassword"
+                                    placeholder="********"
+                                    onChange={handleChange}
+                                />
+                            </FormGroup>
+                        </Col>
+                        <div className='button-cont'><Button type='submit' >Log In</Button> <Link to='/register'><Button>Register</Button></Link></div>
+                    </Form>
+                </Container>
+            </div>
         </>
     )
 }
