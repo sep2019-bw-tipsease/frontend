9ce1e05b2be879b05fe30b6160de06576e7bcbd7
jtmccutcheon <jt.mccutcheon@live.com>
2019-09-23 17:15:48 -0400

    added dashboard and serverpage components

---
+++ tipsease/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <>
+            <h1>imagine a grid here!</h1>
+            <p></p>
+        </>
+    )
+}
+
+export default Dashboard
