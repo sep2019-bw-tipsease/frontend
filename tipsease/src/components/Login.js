2d7bb1eb34b017401e2e8978d8434c4057b2e6c1
jtmccutcheon <jt.mccutcheon@live.com>
2019-09-23 18:27:51 -0400

    added logo refactored home/login/register

--- tipsease/src/components/Login.js
+++ tipsease/src/components/Login.js
@@ -5,55 +5,79 @@ import {
     Button,
 } from 'reactstrap';
 import { Link } from 'react-router-dom'
+import logo from '../images/logo.png'
+import styled from 'styled-components'
 
 const Login = () => {
-
+    const LoginStyle = styled.div`
+        display: flex;
+        flex-direction: column;
+        
+        align-items: center;
+        img {
+            width: 25rem;
+        }
+        .login {
+            width: 30rem;
+            .button-cont {
+                display: flex;
+                justify-content: center;
+                button {
+                    width: 8rem;
+                    background: #6E588A;
+                }
+            }
+      
+        }
+    `;
 
     const userLogin = e => {
-
+        e.preventDefault();
     }
 
     const handleChange = e => {
 
     }
     return (
-        <>
-            <div>
-                <Container className="login">
-                    <h2>Returning User? Sign in here</h2>
-                    <Form className="form" onSubmit={userLogin}>
-                        <Col>
-                            <FormGroup>
 
-                            </FormGroup>
-                            <FormGroup>
-                                <Label>Email</Label>
-                                <Input
-                                    type="text"
-                                    name="username"
-                                    id="exampleEmail"
-                                    placeholder="username"
-                                    onChange={handleChange}
-                                />
-                            </FormGroup>
-                        </Col>
-                        <Col>
-                            <FormGroup>
-                                <Label for="examplePassword">Password</Label>
-                                <Input
-                                    type="password"
-                                    name="password"
-                                    id="examplePassword"
-                                    placeholder="********"
-                                    onChange={handleChange}
-                                />
-                            </FormGroup>
-                        </Col>
-                        <div className='button-cont'><Button type='submit' >Log In</Button> <Link to='/register'><Button>Register</Button></Link></div>
-                    </Form>
-                </Container>
-            </div>
-        </>
+        <LoginStyle>
+            <img src={logo} />
+            <Container className="login">
+
+                <h2>Returning User? Sign in here</h2>
+                <Form className="form" onSubmit={userLogin}>
+                    <Col>
+                        <FormGroup>
+
+                        </FormGroup>
+                        <FormGroup>
+                            <Label>Username</Label>
+                            <Input
+                                type="text"
+                                name="username"
+
+                                placeholder="username"
+                                onChange={handleChange}
+                            />
+                        </FormGroup>
+                    </Col>
+                    <Col>
+                        <FormGroup>
+                            <Label for="examplePassword">Password</Label>
+                            <Input
+                                type="password"
+                                name="password"
+
+                                placeholder="********"
+                                onChange={handleChange}
+                            />
+                        </FormGroup>
+                    </Col>
+                    <div className='button-cont'><Button type='submit' >Log In</Button></div>
+                </Form>
+            </Container>
+        </LoginStyle>
+
     )
 }
 export default Login;
