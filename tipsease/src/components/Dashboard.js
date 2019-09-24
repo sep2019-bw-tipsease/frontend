b0d40f76ff31aa326614b4464764f2c6aad058fd
Bayron Puac <bayronpuac@gmail.com>
2019-09-24 17:58:57 -0500

    Added card to the dashbaord

--- tipsease/src/components/Dashboard.js
+++ tipsease/src/components/Dashboard.js
@@ -1,12 +1,13 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
-import { connect } from 'react-redux'
-import { getWorkers } from './actions'
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import { getWorkers } from './actions';
+import ServerPage from './ServerPage';
+
 
 
 const Dashboard = ({ getWorkers, workers }) => {
-    console.log(workers)
-
+    console.log(workers);
 
     useEffect(() => {
         getWorkers()
@@ -14,17 +15,18 @@ const Dashboard = ({ getWorkers, workers }) => {
 
     return (
         <div>
-            <h1>imagine a grid here!</h1>
-            {workers.map(w => {
-                console.log(w)
-            })}
+        {workers.map(w => {
+            console.log(w)
+            return <ServerPage key={w.id} w={w} />
 
-        </div>
+        })}
+
+    </div>
     )
 }
 
 const mapStateToProps = state => {
-    // console.log(state, 'dashboard state')
+    console.log(state, 'dashboard state')
     return {
         workers: state.workers
     }
