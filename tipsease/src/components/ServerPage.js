2d339ab73643423e6b9ff459117d2e97fd33fe24
jtmccutcheon <jt.mccutcheon@live.com>
2019-09-26 01:20:41 -0400

    added file for netlify

--- tipsease/src/components/ServerPage.js
+++ tipsease/src/components/ServerPage.js
@@ -21,37 +21,10 @@ const ServerPage = props => {
 
 
   return (
-    // <div className="cardContainer">
-    //   {/* <Card className="card">
-    //     <CardBody>
-    //       <CardImg className="Img" src={image} alt="Card image cap" />
-    //       <CardTitle>Username: {username}</CardTitle>
-    //       <CardTitle>Name: {first_name}</CardTitle>
-    //       <CardSubtitle>Last Name: {last_name}</CardSubtitle>
-    //       <CardText>Quote: "{tagline}"</CardText>
-    //       <CardSubtitle>Time: {time}</CardSubtitle>
-    //       <CardText>Total Tip: {tip_total}</CardText>
-    //       <Link to={`/dashboard/${id}`}><Button>Tip Me!</Button></Link>
-    //     </CardBody>
-    //   </Card> */}
-    // </div>
     <div>
       <h1>serverpage</h1>
       <div>
-        {/* {workers.flat(w => {
-          console.log(w)
-          return (
-            <p>hi</p>
-          )
-        })} */}
-        {/* {workers.map(w => {
-          console.log(w)
-        })
 
-          .filter(i => {
-            return i.id === props.match.params.id
-          })
-        } */}
         <h1>{workers.username}</h1>
       </div>
     </div>
